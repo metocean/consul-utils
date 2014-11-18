e73188c64d80148e78cd1edc27023211f4ecc0b8
Thomas Coats <github@voodoolabs.net>
2014-11-19 08:42:27 +1300

    Exposing new functionality

--- index.coffee
+++ index.coffee
@@ -2,3 +2,5 @@ module.exports =
   DiffPool: require './src/diff-pool'
   Watch: require './src/watch'
   Service: require './src/service'
+  KV: require './src/kv'
+  FindReplace: require './src/find-replace'
--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 module.exports = {
   DiffPool: require('./src/diff-pool'),
   Watch: require('./src/watch'),
-  Service: require('./src/service')
+  Service: require('./src/service'),
+  KV: require('./src/kv'),
+  FindReplace: require('./src/find-replace')
 };
